4bc056dd084cf2a18dedad920aaa38d714e0123e
Your Name <94886034+saumyaakumarasinghe@users.noreply.github.com>
2023-06-12 20:12:01 +0530

    set up env-cmd

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -3,7 +3,8 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(' ')[1];  // const token = authHeader ? authHeader.split(' ')[1] : null;
+    // const token = authHeader && authHeader.split(' ')[1];
+    const token = authHeader ? authHeader.split(' ')[1] : null;
     if (token == null) return res.sendStatus(401);  // User have not send a token
 
     const decode = jwt.verify(token, 'advancedNode');  // Verify the given token
